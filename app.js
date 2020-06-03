f4f6fb94e24543e3dfb9ba10d6ede38e1043991e
Anitha Venkatesan <anithamca68@gmail.com>
2020-06-03 18:17:01 -0500

    called the render function

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ inquirer
       message: "What is your role in the team as an employee?",
       name: "role",
       choices : ["Engineer","Intern","Manager"]
-    }]).then(function(response) {
+    }
+    ]).then(function(response) {
       switch(response.role) {
         case "Engineer" :
            inquirer
@@ -34,6 +35,17 @@ inquirer
               validate: (input) => {
                 if (lodash.isEmpty(input)) {
                  return "Employee name is required.";
+                }
+              return true;
+              }
+            },
+            {
+              type: "input",
+              message: "What is your id as an employee?",
+              name: "id",
+              validate: (input) => {
+                if (lodash.isEmpty(input)) {
+                 return "Employee id is required.";
                 }
               return true;
               }
@@ -62,7 +74,7 @@ inquirer
             },
           ]).then (function (engineerResponse){
              console.log(engineerResponse);
-             let newEngineer = new Engineer(engineerResponse.name,engineerResponse.github,engineerResponse.email);
+             let newEngineer = new Engineer(engineerResponse.name,engineerResponse.id,engineerResponse.github,engineerResponse.email);
              console.log(newEngineer);
              employeeArray.push(newEngineer);
              });
@@ -77,6 +89,17 @@ inquirer
               validate: (input) => {
                 if (lodash.isEmpty(input)) {
                  return "Employee name is required.";
+                }
+              return true;
+              }
+            },
+            {
+              type: "input",
+              message: "What is your id as an employee?",
+              name: "id",
+              validate: (input) => {
+                if (lodash.isEmpty(input)) {
+                 return "Employee id is required.";
                 }
               return true;
               }
@@ -105,7 +128,7 @@ inquirer
             },
           ]).then (function (internResponse){
               console.log(internResponse);
-              let newIntern = new Intern(internResponse.name,internResponse.school,internResponse.email);
+              let newIntern = new Intern(internResponse.name,internResponse.id,internResponse.school,internResponse.email);
               console.log(newIntern);
               employeeArray.push(newIntern);
              });
@@ -124,6 +147,17 @@ inquirer
                 return true;
                 }
              },
+             {
+              type: "input",
+              message: "What is your id as an employee?",
+              name: "id",
+              validate: (input) => {
+                if (lodash.isEmpty(input)) {
+                 return "Employee id is required.";
+                }
+              return true;
+              }
+            },
              {
                 type: "input",
                 message: "What is your office number as an employee?",
@@ -148,33 +182,24 @@ inquirer
               },
           ]).then (function (managerResponse){
               console.log(managerResponse);
-              let newManager = new Manager(managerResponse.name,managerResponse.officeNumber,managerResponse.email);
+              let newManager = new Manager(managerResponse.name,managerResponse.id,managerResponse.officeNumber,managerResponse.email);
               console.log(newManager);
               employeeArray.push(newManager);
             }); 
+    }  
+  }).then(async function(data) {
+    try{
+      const renderHtml = await render(employeeArray); 
+      console.log(renderHtml);    
+    }
+    catch(err) {
+      console.log(err);
     }
   });
+ 
+  
+  
+
     
      
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
