1297dc38a97a35d5e3e3b0be6ca54034dbbb93cc
Anitha Venkatesan <anithamca68@gmail.com>
2020-06-05 16:45:16 -0500

    Fixed the manager role issues since have to add only one manager

--- app.js
+++ app.js
@@ -9,134 +9,167 @@ const render = require("./lib/htmlRenderer");
 //asynchronous function for getting user input
 async function getInputsFromUser() {
   const response = await inquirer
-  .prompt([       
-    {
-      type: "list",
-      message: "What is your role in the team as an employee?",
-      name: "role",
-      choices : ["Engineer","Intern","Manager"]
+    .prompt([
+      {
+        type: "list",
+        message: "Add your role in the team as an employee?",
+        name: "role",
+        choices: ["Engineer", "Intern", "I dont want to add anymore employees"]
+      }
+    ]);
+  // If employee.role is ["Engineer", "Intern"] ,then ask questions;
+  if (lodash.includes(["Engineer", "Intern"], response.role)) {
+    const questions = [
+      {
+        type: "input",
+        message: "What is employee name ?",
+        name: "name",
+        validate: (input) => {
+          // lodash validation method for validating user input
+          if (lodash.isEmpty(input)) {
+            return "Employee name is required.";
+          }
+          return true;
+        }
+      },
+      {
+        type: "input",
+        message: "What is id of the employee?",
+        name: "id",
+        validate: (input) => {
+          if (lodash.isEmpty(input)) {
+            return "Employee id is required.";
+          }
+          if (isNaN(input)) {
+            return "Please enter valid number";
+          }
+          return true;
+        }
+      },
+      {
+        type: "input",
+        message: "What is email id of the employee?",
+        name: "email",
+        validate: (input) => {
+          if (lodash.isEmpty(input)) {
+            return "Employee email id is required.";
+          }
+          return true;
+        }
+      }
+    ];
+    switch (response.role) {
+      case "Engineer":
+        questions.push({
+          type: "input",
+          message: "What is GitHub user name of the engineer?",
+          name: "github",
+          validate: (input) => {
+            if (lodash.isEmpty(input)) {
+              return "Employee github user name  is required.";
+            }
+            return true;
+          }
+        });
+        break;
+      case "Intern":
+        questions.push({
+          type: "input",
+          message: "What is school name of the intern?",
+          name: "school",
+          validate: (input) => {
+            if (lodash.isEmpty(input)) {
+              return "Employee school name  is required.";
+            }
+            return true;
+          }
+        });
+        break;
     }
-  ]);
+    const commonResponse = await inquirer.prompt(questions);
+    //returning the user response as an object  
+    return {
+      role: response.role,
+      name: commonResponse.name,
+      id: commonResponse.id,
+      email: commonResponse.email,
+      github: commonResponse.github,
+      school: commonResponse.school
+    };
+  } else {
+    // if the response.role is not ["Engineer", "Intern"] then dont ask questions 
+    return {
+      role: response.role
+    };
+  }
+}
 
-  const questions = [
-    {
-      type: "input",
-      message: "What is your name ?",
-      name: "name",
-      validate: (input) => {// lodash validation method for validating user input
-        if (lodash.isEmpty(input)) {
-         return "Employee name is required.";
-        }
-      return true;
-      }
-    },
-    {
-      type: "input",
-      message: "What is your employee id?",
-      name: "id",
-      validate: (input) => {
-        if (lodash.isEmpty(input)) {
-         return "Employee id is required.";
-        }
-        if (isNaN(input)) {
-          return "Please enter valid number";
-        } 
-      return true;
-      }
-    },
-    {
-      type: "input",
-      message: "What is your email id?",
-      name: "email",
-      validate: (input) => {
-        if (lodash.isEmpty(input)) {
-        return "Employee email id is required.";
-      }
-      return true;
-      }
-    }
-  ];
-  switch(response.role) {
-    case "Engineer" :
-      questions.push( {
+//asynchronous function for getting manager details to add in the team
+async function getManagerInfo() {
+  console.log("Build a team with your team member details");
+  console.log("Add a Manager to your team?");
+  const responseManager = await inquirer
+    .prompt([
+      {
         type: "input",
-        message: "What is your GitHub user name?",
-        name: "github",
+        message: "What is the manager name ?",
+        name: "name",
         validate: (input) => {
-         if (lodash.isEmpty(input)) {
-          return "Employee github user name  is required.";
+          if (lodash.isEmpty(input)) {
+            return "Employee name is required.";
+          }
+          return true;
         }
-        return true;
-        }        
-      });
-      break;
-    case "Intern":
-      questions.push({
+      },
+      {
         type: "input",
-        message: "What is your school name?",
-        name: "school",
+        message: "What is the manager employee id?",
+        name: "id",
         validate: (input) => {
-         if (lodash.isEmpty(input)) {
-          return "Employee school name  is required.";
+          if (lodash.isEmpty(input)) {
+            return "Employee id is required.";
+          }
+          if (isNaN(input)) {
+            return "Please enter valid number";
+          }
+          return true;
         }
-        return true;
-        }
-      });
-      break;
-    case "Manager":
-      questions.push({
+      },
+      {
         type: "input",
-        message: "What is your office number?",
+        message: "What is manager email id?",
+        name: "email",
+        validate: (input) => {
+          if (lodash.isEmpty(input)) {
+            return "Employee email id is required.";
+          }
+          return true;
+        }
+      },
+      {
+        type: "input",
+        message: "What is manager office number?",
         name: "officeNumber",
         validate: (input) => {
           if (lodash.isEmpty(input)) {
             return "Employee office number  is required.";
           }
-        return true;
-        }
-      });
-      break;
-  }
-
-  const commonResponse = await inquirer.prompt(questions);
-  //returning the user response as an object  
-  return {
-    role: response.role,
-    name: commonResponse.name,
-    id: commonResponse.id,
-    email: commonResponse.email,
-    github: commonResponse.github,
-    school: commonResponse.school,
-    officeNumber: commonResponse.officeNumber
-  };
-}
-//asynchronous function for getting number of employees to add in the team
-async function getNumberOfEmployees() {
-  const response = await inquirer
-    .prompt([       
-      {
-        message: "How many employees you want to add in the team?",
-        name: "numberOfEmployees",
-        validate: (input) => {
-          if (lodash.isEmpty(input)) {
-           return "Please enter number of employees to add.";
-          }
-          if (isNaN(input)) {
-            return "Please enter valid number";
-          } 
           return true;
         }
       }
     ]);
 
-  const numberOfEmployees = parseInt(response.numberOfEmployees);
-  const numberOfEmployeesArray = lodash.range(numberOfEmployees);
-  let employeeArray=[];
-  //for of loop for running the numberOfEmployeesArray sequentially
-  for (const count of numberOfEmployeesArray) {
+  let employeeArray = [];
+  const manager = new Manager(responseManager.name, responseManager.id, responseManager.email, responseManager.officeNumber);
+  employeeArray.push(manager);
+  while (true) {
     const employee = await getInputsFromUser();
-
-    switch(employee.role) {//creating new objects for each constructor 
+    // If employee.role is not in ["Engineer", "Intern"] array then break;
+    if (!lodash.includes(["Engineer", "Intern"], employee.role)) {
+      break;
+    }
+    switch (employee.role) {
+      //creating new objects for each role 
       case "Engineer":
         const engineer = new Engineer(employee.name, employee.id, employee.email, employee.github);
         employeeArray.push(engineer);
@@ -145,28 +178,24 @@ async function getNumberOfEmployees() {
         const intern = new Intern(employee.name, employee.id, employee.email, employee.school);
         employeeArray.push(intern);
         break;
-      case "Manager":
-        const manager = new Manager(employee.name, employee.id, employee.email, employee.officeNumber);
-        employeeArray.push(manager);
-        break;
     }
   }
+
+
   const OUTPUT_DIR = path.resolve(__dirname, "output");
   const outputPath = path.join(OUTPUT_DIR, "team.html");
   const renderHtml = render(employeeArray);
-  fs.appendFileSync(outputPath,renderHtml);  
+  fs.appendFileSync(outputPath, renderHtml);
 }
-
 var dir = './output';
 //creating a new directory 'output' for html file
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
 }
 //deleting the output directory everytime before adding the team member
 try {
   fs.unlinkSync('./output/team.html');
-  //console.log('successfully deleted /output/team.html');
 } catch (err) {
   // handle the error
 }
-getNumberOfEmployees();
+getManagerInfo();
